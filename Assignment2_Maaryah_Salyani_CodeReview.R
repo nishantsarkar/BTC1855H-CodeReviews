21faa250bffb6cd7db06eeaacf64352fc5252b8c
Nishant <nsrk01@gmail.comm>
2023-06-16 19:48:59 -0400

    Expanded review; added examples

--- Assignment2_Maaryah_Salyani_CodeReview.R
+++ Assignment2_Maaryah_Salyani_CodeReview.R
@@ -3,6 +3,10 @@
 setwd("~/Desktop/coding_course")
 #I have just set my working directory
 
+#' REVIEW: I would include a short comment at the beginning of the code that clarifies
+#' what the code does. Ex. "A script that takes input numbers and determines whether
+#' or not they are narcissistic."
+
 three_digit <- as.numeric (readline(prompt = "enter a three digit positive number: "))
 
 #I have prompted the user to enter a three digit positive number
@@ -13,9 +17,13 @@ three_digit <- as.numeric (readline(prompt = "enter a three digit positive numbe
 #' inputs - there doesn't appear to be a check for whether the input is a number, and if 
 #' I input letters, the code breaks. This could be ameliorated by checking if the input
 #' is a number using is.numeric and as.numeric, then converting the input to an integer
-#' after it passes those conditions. Furthermore, our style guide says that there should
-#' not be a space between a function call and the opening paranthesis - in this case, 
-#' the as.numeric function.
+#' after it passes those conditions. This could also be integrated into the if statement
+#' below. For example: 
+  #' if ((is.numeric(as.numeric(userinput))) &
+  #'    (nchar(userinput) == 3) &  
+  #'    (as.numeric(userinput) >= 0)) { 
+#' Furthermore, our style guide says that there should not be a space between a function call 
+#' and the opening paranthesis - in this case, the as.numeric() function.
 
 # ifelse statement to check if number is positive and three digits in the first condition 
 if (three_digit > 0 && nchar(three_digit) == 3) { 
@@ -43,7 +51,8 @@ digit_3 <- three_digit %% 10
 #' by using floor division by 100 to isolate the first digit, mod/floor division
 #' to isolate the second, and mod to isolate the third. Another possible way to accomplish
 #' this goal could be to use the strsplit function, which could split the input into
-#' three digits and store them in a vector.
+#' three digits and store them in a vector. For example:
+  #' digits <- as.numeric(strsplit(three_digit, "")[[1]])
 
 sum_of_cubes <- digit_1^3 + digit_2^3 + digit_3^3
 # this will cube each of the separated digits individually, then sum them
@@ -61,7 +70,7 @@ if (three_digit == sum_of_cubes) { #check if the three digit number = sum of the
 #' OVERALL COMMENTS: The code follows a logical structure and is well-commented. You
 #' do a great job of noting down what each piece of code does, so it is easy to follow.
 #' Excellent solution to splitting the input into individual digits, and calculating 
-#' whether the input number is an Armstrong number. However, the code should include a 
-#' check to make sure the user input is numeric: Task 2 in the assignment guidelines. 
-#' Also, make sure to adhere to the style guide set out for this course. 
-#' Overall, great work!
+#' whether the input number is an Armstrong number. Variable names are descriptive and
+#' it is easy to follow along with them. However, the code should include a check to make 
+#' sure the user input is numeric: Task 2 in the assignment guidelines. Also, make sure 
+#' to adhere to the style guide set out for this course. Overall, great work!
