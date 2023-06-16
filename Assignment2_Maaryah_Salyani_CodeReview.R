b282eed9e3e9221b7d3e67f850c3e95a962e74ad
Nishant <nsrk01@gmail.comm>
2023-06-16 19:37:52 -0400

    Reviewed remaining code

--- Assignment2_Maaryah_Salyani_CodeReview.R
+++ Assignment2_Maaryah_Salyani_CodeReview.R
@@ -24,6 +24,10 @@ if (three_digit > 0 && nchar(three_digit) == 3) {
   print("Error..terminating session")
   quit()
 }
+
+#' REVIEW: Good solution to check if the number is positive and three-digit using nchar.
+#' As above, this section should include a check to see if the input is a number or not.
+
 # Check if the number is an Armstrong number
 # will first create variables to extract the individual digits from the three digit number, then sum the cube of each number
 digit_1 <- three_digit %/% 100
@@ -35,6 +39,12 @@ digit_2 <- (three_digit %% 100) %/% 10
 digit_3 <- three_digit %% 10
 #separated the last digit (ones digit) from the three digit number by using the operand to give the remainder when the number is divided by 10 
 
+#' REVIEW: This is a great solution to split the input number into individual digits,
+#' by using floor division by 100 to isolate the first digit, mod/floor division
+#' to isolate the second, and mod to isolate the third. Another possible way to accomplish
+#' this goal could be to use the strsplit function, which could split the input into
+#' three digits and store them in a vector.
+
 sum_of_cubes <- digit_1^3 + digit_2^3 + digit_3^3
 # this will cube each of the separated digits individually, then sum them
 
@@ -44,3 +54,14 @@ if (three_digit == sum_of_cubes) { #check if the three digit number = sum of the
 } else {
   print(paste(three_digit, "is not a narcissistic number."))
 }
+
+#' REVIEW: Good work summing the individual digit cubes then comparing them to the
+#' original input to determine if the output is an Armstrong number.
+#' 
+#' OVERALL COMMENTS: The code follows a logical structure and is well-commented. You
+#' do a great job of noting down what each piece of code does, so it is easy to follow.
+#' Excellent solution to splitting the input into individual digits, and calculating 
+#' whether the input number is an Armstrong number. However, the code should include a 
+#' check to make sure the user input is numeric: Task 2 in the assignment guidelines. 
+#' Also, make sure to adhere to the style guide set out for this course. 
+#' Overall, great work!
